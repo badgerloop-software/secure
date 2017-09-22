3dc389ef743bdb21e4b193ef7ad19a9be7e088f0
Jordan Jones <jordanalanjones@gmail.com>
2017-09-21 22:22:51 -0500

    Commiting to fix badgerloop.com/secure

--- php/classes/SlackAuth.php
+++ php/classes/SlackAuth.php
@@ -8,6 +8,7 @@ class SlackAuth
 	private $clientSecret = "";
 
 	function __construct() {
+		session_start();
 		$secret = new Secrets();
 
 		$this->clientId = $secret->getClientId();
@@ -15,26 +16,40 @@ class SlackAuth
 	}
 
 	public function checkAuthStatus() {
-		if($_SESSION['email'] == null && $_GET['code'] == null) {
-			// Dirty way to do this...
-			echo '<meta http-equiv="refresh" content="0; url=https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=' . $this->clientId . '&redirect_uri=http://localhost:8080">';
-		} elseif ($_GET['code'] !== null) {
-			$this->code = $_GET['code'];
+		if (!$_SESSION['loggedIn'] && is_null($_GET['code'])) {
+			$this->redirect();
+		} elseif ($_GET['code']) {
 			$this->getUserInfo();
 		}
 	}
 
+	private function redirect() {
+		echo '<meta http-equiv="refresh" content="0; url=https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=' . $this->clientId . '&redirect_uri=http://localhost:8080">';
+	}
+
 	private function getUserInfo() {
-		$url = "https://slack.com/api/oauth.access?client_id=" . $this->clientId . "&client_secret=" . $this->clientSecret . "&code=" . $this->code;
+		$url = "https://slack.com/api/oauth.access?client_id=" . $this->clientId . "&client_secret=" . $this->clientSecret . "&code=" . $_GET['code'] . "&redirect_uri=http://localhost:8080";
 //		echo $url;
 		$curl = curl_init();
 
 		curl_setopt($curl, CURLOPT_POST, true);
 		curl_setopt($curl, CURLOPT_URL, $url);
-
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 		$result = curl_exec($curl);
 		curl_close($curl);
 		$this->response = $result;
+		$this->saveSession();
+	}
 
+	private function saveSession() {
+		$json = json_decode($this->response, false);
+		$userName = $json->user->name;
+		$userEmail = $json->user->email;
+		$_SESSION['loggedIn'] = true;
+
+		if(!is_null($userName) && !is_null($userEmail)) {
+			$_SESSION['userName'] = $userName;
+			$_SESSION['userEmail'] = $userEmail;
+		}
 	}
 }
--- php/main.php
+++ php/main.php
@@ -1,13 +1,17 @@
 <?php
 require_once('classes/SlackAuth.php');
+//require_once('classes/Members.php');
 
+//$members = new Members();
 $auth = new SlackAuth();
 
 $auth->checkAuthStatus();
 
-echo $auth->response;
-
 echo "Hello from PHP!";
+
+echo $_SESSION['userEmail'];
+
+//$members->getMemberByEmail();
 
 ?>
 
